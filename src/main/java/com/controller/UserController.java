9ac278e8fef6875d465caf37ab8a6e69c214d0b9
Li <shengyuan.a.li@accenture.com>
2020-11-06 18:04:59 +0800

    增加上传功能

--- src/main/java/com/controller/UserController.java
+++ src/main/java/com/controller/UserController.java
@@ -1,21 +1,37 @@
 package com.controller;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.List;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
 import com.pojo.User;
 import com.service.UserService;
+
 
 @Controller
 public class UserController {
 	@Autowired
 	private UserService userService;
+	private static final Logger LOGGER = LoggerFactory.getLogger(UserController.class);
+	
+	@RequestMapping(value = "/login")
+	public String login() {
+		return "/login";
+	}
 	
 	@RequestMapping(value = "/")
 	public String index(Model model) {
@@ -62,4 +78,29 @@ public class UserController {
 		userService.delete(id);
         return "redirect:/";
 	}
+	
+	@GetMapping("/upload")
+    public String upload() {
+        return "/upload";
+    }
+
+    @PostMapping("/upload")
+    @ResponseBody
+    public String upload(@RequestParam("file") MultipartFile file) {
+        if (file.isEmpty()) {
+            return "上传失败，请选择文件";
+        }
+
+        String fileName = file.getOriginalFilename();
+        String filePath = "C:\\Workspace\\eclipse-workspace\\";
+        File dest = new File(filePath + fileName);
+        try {
+            file.transferTo(dest);
+            LOGGER.info("上传成功");
+            return "上传成功";
+        } catch (IOException e) {
+            LOGGER.error(e.toString(), e);
+        }
+        return "上传失败！";
+    }
 }
